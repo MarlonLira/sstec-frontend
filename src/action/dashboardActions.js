3dba6f41468c64410e859661a39f62375c05347c
Marlon Lira <marlonlira3@gmail.com>
2020-01-22 17:08:37 -0300

    test(production): testando em produção

--- src/action/dashboardActions.js
+++ src/action/dashboardActions.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 import { toastr } from 'react-redux-toastr';
 import { reset as resetForm, initialize } from 'redux-form';
 import { showTabs, selectTab } from './tabActions';
+import Consts from '../consts';
 
-const BASE_URL = 'http://localhost:4001';
+const BASE_URL = Consts.API_URL;
+//const BASE_URL = 'http://localhost:4001';
 
 export function getSummary() {
   return new Promise((resolve, reject) => {
--- webpack.config.js
+++ webpack.config.js
@@ -3,43 +3,47 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/index.jsx',
-  output:{
+  output: {
     path: __dirname + '/public',
     filename: './app.js'
   },
-  devServer:{
+  devServer: {
     port: process.env.PORT || 8080,
     contentBase: './public'
   },
-  resolve:{
+  resolve: {
     extensions: ['', '.js', '.jsx'],
-    alias:{
+    alias: {
       modules: __dirname + '/node_modules',
       jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
       bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
     }
   },
-  plugins:[
+  plugins: [
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
       'window.jQuery': 'jquery'
     }),
-    new ExtractTextPlugin('app.css')
+    new ExtractTextPlugin('app.css'),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.optimize.UglifyJsPlugin()
   ],
-  module:{
+  module: {
     loaders: [{
       test: /.js[x]?$/,
       loader: 'babel-loader',
       exclude: /node_modules/,
-      query:{
+      query: {
         presets: ['es2015', 'react'],
         plugins: ['transform-object-rest-spread']
       }
-    },{
+    }, {
       test: /\.css$/,
       loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-    },{
+    }, {
       test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
       loader: 'file'
     }]
