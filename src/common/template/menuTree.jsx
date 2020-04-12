2f4b84b07c51feeece6a18cfaa01861ad999a0eb
Marlon Lira <marlonlira3@gmail.com>
2020-04-11 21:11:13 -0300

    fix(menu)

--- src/common/template/menuTree.jsx
+++ src/common/template/menuTree.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default props => (
   <li className='nav-item has-treeview menu'>
-    <a href className='nav-link'>
+    <a href='' className='nav-link'>
       <i className={`nav-icon fa fa-${props.icon}`}></i>
       <p> {props.label}
         <i className={`right fas fa-angle-left`}></i>
