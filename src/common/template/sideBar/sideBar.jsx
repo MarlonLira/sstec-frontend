e126983c592401aa25b920a1069f5114e4db28de
Marlon Lira <marlonlira3@gmail.com>
2020-04-28 22:14:25 -0300

    style(sidebar): ajustes na cor do sidebar

--- src/common/template/sideBar/sideBar.jsx
+++ src/common/template/sideBar/sideBar.jsx
@@ -4,7 +4,7 @@ import UserPanel from './userPanel';
 import Logo from './logo';
 
 export default props => (
-  <aside className='main-sidebar sidebar-light-primary elevation-4'>
+  <aside className='main-sidebar sidebar-light-primary elevation-4 sidebar-custom'>
     <Logo text="Simple Parking" icon="car" />
     <section className='sidebar'>
       <UserPanel />
