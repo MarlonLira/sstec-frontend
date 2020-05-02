73179e14edc2df746c403da9535ad94d7438100e
Marlon Lira <marlonlira3@gmail.com>
2020-05-01 21:23:33 -0300

    fix(css)

--- src/parking/parkingActions.js
+++ src/parking/parkingActions.js
@@ -8,7 +8,7 @@ import Consts from '../consts';
 const BASE_URL = Consts.API_URL;
 const CURRENT_DATE = GetDateNow().FullDate;
 const INITIAL_VALUES = {};
-const COMPANY_ID = JSON.parse(localStorage.getItem('_sp_company')).id;
+const COMPANY_ID = localStorage.getItem('_sp_company') == null ? 0 : JSON.parse(localStorage.getItem('_sp_company')).id;
 
 export function getList() {
   return new Promise((resolve) => {
