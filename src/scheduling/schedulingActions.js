55cfb736cc605d2ef2c4df8b23c1c59683dee48c
Marlon Lira <marlonlira3@gmail.com>
2020-05-03 02:30:36 -0300

    fix(scheduling): correção do scheduling

--- src/scheduling/schedulingActions.js
+++ src/scheduling/schedulingActions.js
@@ -7,10 +7,11 @@ import Consts from '../consts';
 const BASE_URL = Consts.API_URL;
 const CURRENT_DATE = GetDateNow().FullDate;
 const INITIAL_VALUES = {};
+const COMPANY_ID = localStorage.getItem('_sp_company') == null ? 0 : JSON.parse(localStorage.getItem('_sp_company')).id;
 
 export function getList() {
   return new Promise((resolve) => {
-    axios.get(`${BASE_URL}/schedulings`)
+    axios.get(`${BASE_URL}/schedulings/${COMPANY_ID}`)
       .then(request => {
         resolve({
           type: 'SCHEDULING_FETCHED',
